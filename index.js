5b33ca79c9b5d501a3ffd512538c34c34f98f1ca
krishh <krishshah200622@gmail.com>
2024-12-21 07:55:14 -0800

    project setup with Zero Stge

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const Port = 8080;
+
+app.set("view engine","ejs");
+
+
+
+app.listen(Port,(err)=>{
+    if(err){
+        throw Error;
+    }
+    console.log(`server are Running on Port ${Port}`);
+})
