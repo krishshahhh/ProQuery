f965a6a3bd04e4ce873e56f13010fd7af6f378ed
krishh <krishshah200622@gmail.com>
2024-12-23 01:26:56 -0800

    build proqury home pg

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const app = express();
 const Port = 8080;
 
 app.set("view engine","ejs");
-app.use(express.static(Path.join(__dirname,"public")))
+// app.use(express.static(path.join(__dirname,"public")))
 
-
+app.get('/',(req,res)=>{
+    res.render("home")
+})
 
 app.listen(Port,(err)=>{
     if(err){
