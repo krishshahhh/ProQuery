fa7fcef4c0f3620b1a54741a476c76810ee341b1
krishh <krishshah200622@gmail.com>
2024-12-28 02:54:59 -0800

    build some rest api for post creation + add some style of web pg

--- index.js
+++ index.js
@@ -5,33 +5,76 @@ const app = express();
 const Port = 8080;
 
 app.set("view engine","ejs");
-app.set(express.static(path.join(__dirname,"public")))
-app.use(express.urlencoded({extended:true}))
+app.set(express.static(path.join(__dirname,"public")));
+app.use(express.urlencoded({extended:true}));
 
-app.get('/home',(req,res)=>{
+app.get('/',(req,res)=>{
     res.render("home")
 })
 
-let users = [];
+let usersDB = [
+    {
+        user:"krish",
+        pass:12345678
+    }
+];
 
+let posts = [
+    {
+        username:"kehsv",
+        Content:"how to start trading"
+},
+    {
+        username:"girdhri",
 
-app.get("/signin",function(req,res){
+        Content:"how to start trading"
+},
+    {
+        username:"mohit",
+        Content:"how to start trading"
+}
+
+];
+
+app.get("/posts",(req,res)=>{
+    res.render("post",{posts});
+});
+
+app.get("/posts/n",(req,res)=>{
+    res.render("create-post");
+});
+
+app.post("/posts", (req, res) => {
+    let { username, Content } = req.body;
+    posts.push({ username, Content });
+    res.redirect("/posts");
+});
+
+app.get("/signin",(req,res)=>{
     
     res.render("signin");
     console.log(info);
 });
 
-app.get("/login",function(req,res){
+app.get("/login",(req,res)=>{
     res.render("login");
+})
+
+app.post("/login",(req,res)=>{
+        let {username,password} = req.body
+        let userfound = usersDB.find(u => u.user === username && u.pass === password)
+
+
+        if(userfound){
+            res.redirect("/posts")
+            
+        }
+        else{
+            // res.sendStatus(404);
+        }
 });
 
-app.get("/create-post",function(req,res){
-    res.render("create-post");
-});
 
-app.get("/post",function(req,res){
-    res.render("post");
-});
 
 app.listen(Port,(err)=>{
     if(err){
