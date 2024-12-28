e70d7954a44cd32f4d96dd97b60591caa0040ec0
krishh <krishshah200622@gmail.com>
2024-12-28 04:53:56 -0800

    build single pg see feature for proqury

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const Port = 8080;
+const {v4:uuid} = require("uuid");
 
 app.set("view engine","ejs");
+app.set("views", "./views");
 app.set(express.static(path.join(__dirname,"public")));
 app.use(express.urlencoded({extended:true}));
 
@@ -21,15 +23,17 @@ let usersDB = [
 
 let posts = [
     {
+        id:uuid(),
         username:"kehsv",
         Content:"how to start trading"
 },
     {
+        id:uuid(),
         username:"girdhri",
-
         Content:"how to start trading"
 },
     {
+        id:uuid(),
         username:"mohit",
         Content:"how to start trading"
 }
@@ -46,9 +50,16 @@ app.get("/posts/n",(req,res)=>{
 
 app.post("/posts", (req, res) => {
     let { username, Content } = req.body;
-    posts.push({ username, Content });
+    let id = uuid();
+    posts.push({ id ,username, Content });
     res.redirect("/posts");
 });
+
+app.get("/posts/:id",(req,res)=>{
+    let id  = req.params.id
+    let post = posts.find((p) => id === p.id);
+    res.render("seepost",{post})
+})
 
 app.get("/signin",(req,res)=>{
     
